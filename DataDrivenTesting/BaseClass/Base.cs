28ad3e20f45396a8de8bc9ba7463c57bc587c7f1
Vivek Guntuku <vivek.g@tvsnext.io>
2021-09-14 13:01:36 +0530

    [Automating login function using datadriven method]

---
+++ DataDrivenTesting/BaseClass/Base.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+
+namespace DataDrivenTesting.BaseClass
+{
+    public class Base
+    {
+        public static IWebDriver driver;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://www.facebook.com/";
+
+
+
+            //Implicit wait
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(8000);
+
+
+
+            //Explicit wait
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(1000));
+            wait.IgnoreExceptionTypes(typeof(NoSuchElementException));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2000);
+            driver.Quit();
+        }
+    }
+}
---
+++ DataDrivenTesting/ReadData.cs
@@ -0,0 +1,26 @@
+﻿
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+
+namespace DataDrivenTesting
+{
+     public class ReadDataFromFile : BaseClass.Base
+    {
+        [Test]
+        public void ReadingDataFromExcelTestMethod()
+        {
+            AccessExcelData.PopulateInCollection(@"C:\Users\vivek.g\source\repos\DataDrivenTesting\DataDrivenTesting\TestData.xlsx");
+            driver.FindElement(By.Name("email")).SendKeys(AccessExcelData.ReadData(1, "Username"));
+            System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.Name("pass")).SendKeys(AccessExcelData.ReadData(1, "Password"));
+            System.Threading.Thread.Sleep(2000);
+            driver.FindElement(By.Name("login")).Click();
+            System.Threading.Thread.Sleep(8000);
+
+
+        }
+
+
+    }
+}
